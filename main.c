fc9666f01fb0addb63e0b07cf12a80f0f9639742
Ayman <87582743+mastergive514@users.noreply.github.com>
2024-02-18 17:58:26 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,216 @@
+// Since we are building an external plugin dll, we need to import from ClassiCube lib instead of exporting these
+#ifdef CC_BUILD_WIN
+// need to specifically declare as imported for Visual Studio
+#define CC_API __declspec(dllimport)
+#define CC_VAR __declspec(dllimport)
+#else
+#define CC_API
+#define CC_VAR
+#endif
+
+
+
+#include <time.h>
+#include "src/Entity.h"
+#include "src/Chat.h"
+#include "src/String.h"
+#include "src/Stream.h"
+#include "src/Platform.h"
+#include "src/Event.h"
+#include "src/Game.h"
+#include "src/Logger.h"
+#include "src/Server.h"
+#include "src/World.h"
+#include "src/Inventory.h"
+#include "src/Window.h"
+#include "src/Graphics.h"
+#include "src/Funcs.h"
+#include "src/Block.h"
+#include "src/EnvRenderer.h"
+#include "src/Utils.h"
+#include "src/TexturePack.h"
+#include "src/Options.h"
+#include "src/Drawer2D.h"
+#include "src/NOVACRAFT.h"
+#include "src/Commands.h"
+#include "src/Vectors.h"
+#include "src/Vorbis.h"
+#include "src/Utils.h"
+#include "src/Server.h"
+
+#define novacraftrelease 0.1
+ 
+/*########################################################################################################################*
+*---------------------------------------------------Plugin implementation-------------------------------------------------*
+*#########################################################################################################################*/
+
+#define SendChat(msg) const static cc_string str = String_FromConst(msg); Chat_Add(&str);
+
+static void TestCommand_Execute(const cc_string* args, int argsCount) {
+    Window_ShowDialog("TEST", "Just a test");
+}
+
+static struct ChatCommand TestCmd = {
+	"Test", TestCommand_Execute, false,
+	{
+		"&a/client Test",
+		"&eExports current map to the schematic file format, as [filename].schematic.",
+		"&eThis can then be imported into software such as MCEdit.",
+	}
+};
+
+
+static void VersionCommand_Execute(const cc_string* args, int argsCount) {
+    SendChat(novacraftrelease);
+}
+
+static struct ChatCommand VersionCmd = {
+	"Version", VersionCommand_Execute, false,
+	{
+		"&a/client Version",
+		"",
+		"",
+	}
+};
+
+
+static void CpeTestCommand_Execute(const cc_string* args, int argsCount) {
+    cc_string message = String_FromConst("Hello, World!");
+
+    Chat_AddOf(&message, MSG_TYPE_ANNOUNCEMENT);
+    Chat_AddOf(&message, MSG_TYPE_BIGANNOUNCEMENT);
+    Chat_AddOf(&message, MSG_TYPE_SMALLANNOUNCEMENT);
+    Chat_AddOf(&message, MSG_TYPE_STATUS_1);
+    Chat_AddOf(&message, MSG_TYPE_STATUS_2);
+    Chat_AddOf(&message, MSG_TYPE_STATUS_3);
+    Chat_AddOf(&message, MSG_TYPE_BOTTOMRIGHT_1);
+    Chat_AddOf(&message, MSG_TYPE_BOTTOMRIGHT_2);
+    Chat_AddOf(&message, MSG_TYPE_BOTTOMRIGHT_3);
+    Chat_Add(&message);
+}
+
+static struct ChatCommand CpeTestCmd = {
+	"CpeTest", CpeTestCommand_Execute, false,
+	{
+		"&a/client CpeTest",
+		"&ePrints 'Hello, World!' with different message types",
+		"",
+	}
+};
+
+
+static void WeatherCommand_Execute(const cc_string* args, int argsCount) {
+    if (argsCount < 1) {
+        SendChat("&eUsage: /weather [Sunny/Rainy/Snowy]");
+        return;
+    }
+
+    if (String_CaselessEqualsConst(&args[0], "Sunny")) {
+        Env_SetWeather(0);
+        SendChat("&eWeather changed to &aSunny");
+    } else if (String_CaselessEqualsConst(&args[0], "Rainy")) {
+        Env_SetWeather(1);
+	SendChat("&eWeather changed to &aRainy");   
+		} else if (String_CaselessEqualsConst(&args[0], "Snowy")) {
+        Env_SetWeather(2);
+        SendChat("&eWeather changed to &aSnowy");
+	   
+		}
+    else {
+        SendChat("Invalid weather type. Please use Sunny, Rainy, or Snowy.");
+    }
+}
+
+static struct ChatCommand WeatherCmd = {
+    "Weather", WeatherCommand_Execute, false,
+    {
+        "&a/client weather [Sunny/Rainy/Snowy]",
+        "&eChange The Weather :D",
+    }
+};
+
+
+
+static void ClearCommand_Execute(const cc_string* args, int argsCount) {
+    cc_string msg = String_FromConst("");
+    for (int i = 1; i < 20; i++) {Chat_Add(&msg);}
+    Chat_Add(&msg);
+    SendChat("&cChat Cleared!");
+
+
+}
+
+static struct ChatCommand ClearCmd = {
+    "Clear", ClearCommand_Execute, false,
+    {
+        		"&a/client clear",
+		"&eAlternative Clear Cmd (From MCGalaxy)",
+    }
+};
+
+static void HacksCommand_Execute(const cc_string* args, int argsCount) {
+    struct LocalPlayer* p = (struct LocalPlayer*)Entities.List[255];
+
+    // Check if the correct number of arguments is provided.
+    if (argsCount != 1) {
+        SendChat("&e/Hacks: &cTrue or False!!");
+        return;
+    }
+
+    if (String_CaselessEqualsConst(&args[0], "true")) {
+	p->Hacks.hanFly            = true;
+	p->Hacks.hanNoclip         = true;
+	p->Hacks.hanSpeed          = true;
+	p->Hacks.hanRespawn        = true;
+	p->Hacks.hanUseThirdPerson = true;
+        SendChat("&eHacks Enabled!"); 
+  }
+   else if (String_CaselessEqualsConst(&args[0], "false")) {
+       
+	p->Hacks.hanFly            = false;
+	p->Hacks.hanNoclip         = false;
+	p->Hacks.hanSpeed          = false;
+	p->Hacks.hanRespawn        = false;
+	p->Hacks.hanUseThirdPerson = false;
+        SendChat("&eHacks Disabled!");
+
+}
+  else {
+        SendChat("&e/Hacks: &cInvalid argument. Use True or False.");
+    }
+}
+
+static struct ChatCommand HacksCmd = {
+    "Hacks", HacksCommand_Execute, false,
+    {
+        "/hacks (True/False)",
+        "&cNote: &eOnly in Singleplayer",
+    }
+};
+
+static void NovaCraft_Init(void) {
+	Commands_Register(&CpeTestCmd);
+    Commands_Register(&HacksCmd);
+    Commands_Register(&ClearCmd);
+	Commands_Register(&WeatherCmd);
+    Commands_Register(&TestCmd);
+}
+
+
+/*########################################################################################################################*
+*----------------------------------------------------Plugin boilerplate---------------------------------------------------*
+*#########################################################################################################################*/
+// Since we are building an external plugin dll, we need to import from ClassiCube lib instead of exporting these
+#ifdef CC_BUILD_WIN
+// special attribute to get symbols exported with Visual Studio
+#define PLUGIN_EXPORT __declspec(dllexport)
+#else
+// public symbols already exported when compiling shared lib with GCC
+#define PLUGIN_EXPORT
+#endif
+
+PLUGIN_EXPORT int Plugin_ApiVersion = 1;
+PLUGIN_EXPORT struct IGameComponent Plugin_Component = {
+	NovaCraft_Init /* Init */
+};
+
